9afae11296f39e1a1fc1d827d540f863b27341a1
Maksim Pushkarev <mpush.dev@gmail.com>
2024-08-19 19:22:26 +0800

    add test_gendiff

---
+++ tests/test_gendiff.py
@@ -0,0 +1,13 @@
+from gendiff.scripts.gendiff import generate_diff
+
+
+with open('tests/fixtures/output/JSON/result.txt', "r") as test_file:
+    result_from_file = test_file.read()
+
+
+PATH1 = 'tests/fixtures/input/JSON/file1.json'
+PATH2 = 'tests/fixtures/input/JSON/file2.json'
+
+
+def test_gendiff():
+    assert generate_diff(PATH1, PATH2) == result_from_file
